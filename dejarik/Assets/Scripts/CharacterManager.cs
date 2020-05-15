bd3561cafa471a518d3f9740c85743a477f6e30a
thalbern <bethalha@microsoft.com>
2020-05-15 14:48:26 +0100

    added character manager singleton to scene

---
+++ dejarik/Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    public static CharacterManager instance = null; //singleton instance
+
+    public GameObject board = null;
+
+    public GameObject ghhhk = null;
+    public GameObject grimtaash = null;
+    public GameObject houjix = null;
+    public GameObject kLorSlug = null;
+    public GameObject kintanStrider = null;
+    public GameObject mantellianSavrip = null;
+    public GameObject monnok = null;
+    public GameObject ngok = null;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
